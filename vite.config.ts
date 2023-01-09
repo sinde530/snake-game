7d9a4b567a1fbb79cd6a8e234e52066f1325c6c2
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-01-09 17:57:45 +0900

    test1

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from "vite-tsconfig-paths"
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base:'/snake-game',
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    assetsInlineLimit: 0,
+  }
 })
